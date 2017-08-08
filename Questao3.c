7efc673bbe4cbdad265b66aca3475960e877eef9
Pablo <pablosccp@live.com>
2017-08-08 10:22:52 -0300

    Questao 3 concluída

---
+++ Questao3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int Sequencia(int i)
+{
+    int j,k;
+
+    for (j=1; j<=i; j++)
+    {
+        for(k=1; k<=j; k++)
+            printf("%d",k);
+        puts("");
+
+    }
+    return 1;
+}
+
+
+int main()
+
+{
+    int valor;
+
+    printf("Informe um valor para a sequencia: ",valor);
+    scanf("%d",&valor);
+
+    Sequencia(valor);
+
+
+
+
+    return 0;
+}
