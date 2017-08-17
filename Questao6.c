e6184a2c2a8c0e43c3a6c8c3e24b19c0ac0feffa
Gustavo Rocha <gustavo_roc@live.com>
2017-08-16 21:01:28 -0400

    Pequena alteração

--- Questao6.c
+++ Questao6.c
@@ -5,55 +5,41 @@
 int main(void){
 
     char telefone[10], novo_Telefone[10];
-    int i, j=1, digitos=0;
+    int i = 0, j = 1, digitos = 0;
 
     printf("Informe o telefone: ");
-    fgets(telefone,10,stdin);
+	scanf("%s", telefone);
 
-    for(i=0; i<strlen(telefone)-1; i++)
-    {
-        if((telefone[i] != '-')){
-
+    for(i=0; i < strlen(telefone); i++){
+        if(telefone[i] != '-'){
             digitos++;
         }
     }
-
-
-    if(digitos == 7)
-    {
+    
+    if(digitos == 7){
         novo_Telefone[0] = '3';
-        for(i=0; i<8; i++)
-        {
-            if(telefone[i] == '-')
-            {
+        for(i=0; i<8; i++){
+            if(telefone[i] == '-'){
                 i++;
             }
+            
             novo_Telefone[j] = telefone[i];
             j++;
-
         }
-    }else if(digitos == 8)
-          {
+    }else if(digitos == 8){
             j = 0;
 
-                for(i=0; i<8; i++)
-                {
+                for(i=0; i<9; i++){
                     if(telefone[i] == '-'){
                     i++;
                     }
                     novo_Telefone[j] = telefone[i];
                     j++;
-
                }
-
           }
 
     printf("Numero corrigido: \n");
-    for(i=0; i<8; i++)
-    {
-
-        printf("%c",novo_Telefone[i]);
-    }
+    printf("%s\n", novo_Telefone);
 
     return 0;
 }
